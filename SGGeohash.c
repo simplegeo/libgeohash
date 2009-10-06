2117270af9787734bcf3efc427b12a52bbe52673
dsmith <derek@crashcorp.com>
2009-10-06 14:36:53 -0600

    Encoding and decoding work properly

--- SGGeohash.c
+++ SGGeohash.c
@@ -47,8 +47,62 @@ unsigned int IndexForChar(char c) {
 
 char* SGGeohashEncode(double lat, double lng, int precision) {
     
+    if(precision < 1 || precision > 20)
+        precision = 10;
     
-    return NULL;
+    char* hash = NULL;
+    
+    if(lat <= 90.0 && lat >= -90.0 && lng <= 180.0 && lng >= -180.0) {
+        
+        hash = (char*)malloc(sizeof(char) * precision + 1);
+        hash[precision] = '0';
+        
+        precision *= 5.0;
+        
+        SGInterval latInterval = {MAX_LAT, MIN_LAT};
+        SGInterval lngInterval = {MAX_LONG, MIN_LONG};
+        SGInterval* interval;
+        double coord, mid;
+        int isEven = 1;
+        unsigned int hashChar = 0;
+        for(int i = 1; i <= precision; i++) {
+         
+            if(isEven) {
+            
+                interval = &lngInterval;
+                coord = lng;                
+                
+            } else {
+                
+                interval = &latInterval;
+                coord = lat;   
+            }
+            
+            mid = (interval->low + interval->high) / 2.0;
+            hashChar = hashChar << 1;
+            
+            if(coord > mid) {
+                
+                interval->low = mid;
+                hashChar |= 0x01;
+                
+            } else
+                interval->high = mid;
+            
+            if(!(i % 5)) {
+             
+                hash[i / 5] = charMap[hashChar];
+                hashChar = 0;
+
+            }
+            
+            isEven = !isEven;
+        }
+     
+        
+    }
+    
+    return hash;
 }
 
 SGGeoCoord SGGeohashDecode(char* hash) {
@@ -64,45 +118,36 @@ SGGeoCoord SGGeohashDecode(char* hash) {
             unsigned int charMapIndex;
             SGInterval latInterval = {MAX_LAT, MIN_LAT};
             SGInterval lngInterval = {MAX_LONG, MIN_LONG};
-            SGInterval* interval, *firstInterval, *secondInterval;
+            SGInterval *interval;
         
+            int isEven = 1;
             double delta;
             for(int i = 0; i < charAmount; i++) {
             
                 charMapIndex = IndexForChar(hash[i]);
-            
-                if(i % 2) {
                 
-                    firstInterval = &latInterval;
-                    secondInterval = &lngInterval;
-                
-                } else {
-                
-                    firstInterval = &lngInterval;
-                    secondInterval = &latInterval;
-                }
-            
+                if(charMapIndex < 0)
+                    break;
             
                 // Interpret the last 5 bits of the integer
                 for(int j = 0; j < 5; j++) {
                 
-                    interval = j % 2 ? secondInterval : firstInterval;
+                    interval = isEven ? &lngInterval : &latInterval;
                 
                     delta = (interval->high - interval->low) / 2.0;
                 
-                    if((charMapIndex << j) & 0x00000010)
+                    if((charMapIndex << j) & 0x0010)
                         interval->low += delta;
                     else
                         interval->high -= delta;
                 
-                    printf("%s %f %f %i %i\n", interval == &latInterval ? "Lat" : "Lng", interval->high, interval->low, charMapIndex, charMapIndex << j);
-                
+                    isEven = !isEven;
                 }
             
             }
         
-            coordinate.latitude = latInterval.high - (latInterval.high - latInterval.low);
-            coordinate.longitude = lngInterval.high - (lngInterval.high - lngInterval.low);
+            coordinate.latitude = latInterval.high - ((latInterval.high - latInterval.low) / 2.0);
+            coordinate.longitude = lngInterval.high - ((lngInterval.high - lngInterval.low) / 2.0);
         }
     }
     
--- SGGeohashTest.c
+++ SGGeohashTest.c
@@ -8,13 +8,35 @@
 
 #include "SGGeohash.h"
 
+#include <string.h>
 #include <stdio.h>
+
+void checkCoords(SGGeoCoord coord, SGGeoCoord expected) {
+ 
+    if(coord.latitude != expected.latitude || coord.longitude != expected.longitude)
+        printf("Error: Expected lat = %.11f and long = %.11f. (%.11f, %.11f)\n", expected.latitude,
+                expected.longitude, coord.latitude, coord.longitude);
+    
+}
+
+void checkHashes(char* hash, char* expected) {
+    
+    if(!strcmp(hash, expected))
+        printf("Error: Expected hash = %s. (%s)", hash, expected);
+}
 
 int main() {
 
     SGGeoCoord coord = SGGeohashDecode("ezs42");
-    if(coord.latitude != 42.6 || coord.longitude == -5.6)
-        printf("Error: Expected lat = 42.6 and long -5.6. (%f, %f)\n", coord.latitude, coord.longitude);
+    SGGeoCoord expectedCoord = {42.60498046875, -5.60302734375};
+    checkCoords(coord, expectedCoord);
+    
+    coord = SGGeohashDecode("ezs42gx");
+    expectedCoord = (SGGeoCoord){42.62489318848, -5.58174133301};
+    checkCoords(coord, expectedCoord); 
+    
+    char* hash = SGGeohashEncode(42.60498046875, -5.60302734375, 5);
+    checkHashes(hash, "ezs42");
 
     return 0;
 }
